b76aef90fcf11c01fce4a59fcc6155b237ccd397
DamienA <alves.d24@laposte.net>
2025-07-03 13:31:01 +0200

    feat(global): add note model

---
+++ app/src/main/java/com/openclassrooms/notes/model/Note.kt
@@ -0,0 +1,8 @@
+package com.openclassrooms.notes.model
+
+/**
+ *  Note represent a note.
+ *  @param title The title of the note.
+ *  @param body The body of the note.
+ */
+class Note (title: String, body: String)
