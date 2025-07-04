56effae04be3f270b90f262ef6b01e350b7d01ce
DamienA <alves.d24@laposte.net>
2025-07-04 08:50:35 +0200

    chore(global)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.openclassrooms.notes"
-    compileSdk = 33
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.openclassrooms.notes"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
